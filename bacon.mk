451f73b9ac09e5fff0a96b0046d4a032e32e6933
Bruno Martins <bgcngm@gmail.com>
2019-09-09 16:43:30 +0200

    bacon: Set shipping API level in the common product makefile
    It's not meant to be set as a board config flag. Simply do it by
    inheriting the makefile that now also exists for devices launched
    with KitKat.
    
    Change-Id: I2ca1f9d207be90c58495b3e20c5e2275e09b18bc

--- bacon.mk
+++ bacon.mk
@@ -15,6 +15,9 @@
 # limitations under the License.
 #
 
+# Inherit shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_k.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
