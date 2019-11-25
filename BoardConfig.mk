29495442ed94920b5dda0eece21ace2ff921b352
Daniel Hernández <danhernan23i@gmail.com>
2019-11-25 04:15:17 -0400

    BoardConfig: Look for the current stock defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_CMDLINE += androidboot.btmacaddr=00:00:00:00:00:00
-TARGET_KERNEL_CONFIG := lineageos_bacon_defconfig
+TARGET_KERNEL_CONFIG := bacon_defconfig
 TARGET_KERNEL_SOURCE := kernel/oppo/msm8974
 
 # Assert
