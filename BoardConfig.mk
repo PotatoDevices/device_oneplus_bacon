58294057f32575aa87df3aeacde0264ab3287f57
Daniel Hernández <danhernan23i@gmail.com>
2020-01-12 22:05:55 -0400

    BoardConfig: Change kernel source path
    * Look for kernel in kernel/oneplus instead of kernel/oppo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_CMDLINE += androidboot.btmacaddr=00:00:00:00:00:00
 TARGET_KERNEL_CONFIG := bacon_defconfig
-TARGET_KERNEL_SOURCE := kernel/oppo/msm8974
+TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := bacon,A0001
